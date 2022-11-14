fa0f7756374839ecb0ce84125103be88decf161c
LucasRuiz00 <80957960+LucasRuiz00@users.noreply.github.com>
2022-11-14 10:04:17 -0300

    Add files via upload

---
+++ programacion-fisica-main/README.md
@@ -0,0 +1,3 @@
+# programacion-fisica
+
+transcripcion del archivo .py a javascript y a partir de eso hacer una pagina web con los respectivos funcionamientos y calculos que requiere sin alerts ni eventos
---
+++ programacion-fisica-main/main/index.js
@@ -0,0 +1,43 @@
+var a, f, m, ffd, ffe, fuerzaNeta, fx, g, inc, material, pesoy, ud, ue;
+
+
+g = 9.8;
+
+function capmasa() {
+    var m = document.getElementById("masa").value;
+    console.log(m);
+    pesoy = m * g;
+}
+
+function capfuerza() {
+    var f = document.getElementById("fuerza").value;
+    console.log(f); 
+    fx = f;
+    
+}
+
+var checkp = document.getElementById("planoS");
+var checkf = document.getElementById("friccionS");
+
+var grados = document.getElementById("gradosp");
+
+
+
+checkp.addEventListener('click' , function() {
+    if (checkp.checked) {
+        grados.innerHTML = `<h2>cuantos grados esta inclinado el plano? </h2>
+        <input type="number" name="numgrados" placeholder="ingrese la inclinacion del plano" id="gradosS">
+        <button type="button" onclick="capgrados()">hecho </button>   `
+        
+    } else{
+        grados.innerHTML = ``
+    }
+    
+});
+
+function capgrados() {
+    var gr = document.getElementById("gradosS");
+    console.log(gr);
+    
+}
+
